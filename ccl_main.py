bc8c271ce7f5dae9a104b961ec975709c5ec7e66
Corey Watson <jcw931@msstate.edu>
2017-11-20 19:35:29 -0600

    Add files via upload

--- ccl_main.py
+++ ccl_main.py
@@ -7,4 +7,12 @@ app = Flask(__name__)
 app.json_encoder.default = lambda self, o: o.to_joson()
 
 # Array to store the objects
-InventoryArr = {};
+InventoryArr = {}
+
+@app.route('/inventory', methods=['GET'])
+
+@app.route('/inventory/location/< location >', methods=['GET'])
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 80)
