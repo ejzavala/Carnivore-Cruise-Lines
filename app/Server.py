f338fd4bb1de57ab5083daf8968de6a889322e13
Corey Watson <jcw931@msstate.edu>
2017-12-01 18:56:23 -0600

    Added revenue

--- app/Server.py
+++ app/Server.py
@@ -11,13 +11,19 @@ app.app_context()
 def add_to_history(cruise_item_idnum):
     conn = db_connect.connect()
     queryhistory = conn.execute("SELECT numberSold FROM cruiseHistory WHERE itemID = '%s'"%(cruise_item_idnum))
+    queryRevenue = conn.execute("SELECT revenue FROM cruiseHistory WHERE itemID = '%s'" % (cruise_item_idnum))
     numberactuallysold = queryhistory.cursor.fetchall()
+    currRev = queryRevenue.cursor.fetchall()
+    queryCost = conn.execute("SELECT cost FROM cruiseItem WHERE itemID = '%s'" % (cruise_item_idnum))
+    itemCost = queryCost.cursor.fetchall()
     if len(numberactuallysold) != 0:
         sold = numberactuallysold[0][0]
         sold = (sold + 1)
+        newRev = (itemCost[0][0] + currRev[0][0])
         query = conn.execute("UPDATE cruiseHistory SET numberSold = (?) WHERE itemID = (?)",(sold, cruise_item_idnum))
+        query = conn.execute("UPDATE cruiseHistory SET revenue = (?) WHERE itemID = (?)", (newRev, cruise_item_idnum))
     else:
-        query = conn.execute("INSERT INTO cruiseHistory (itemID, numberSold) VALUES ('%s', 1)" %str(cruise_item_idnum))
+        query = conn.execute("INSERT INTO cruiseHistory (itemID, numberSold, revenue) VALUES ( ?, ?, ?)", (cruise_item_idnum, 1, itemCost[0][0]))
 
 #changes cruiseItem Table
 def put_changeAvail(cruise_item_id):
@@ -68,7 +74,7 @@ def get_cruisehistory():
     conn = db_connect.connect()  # connect to database
     query = conn.execute("select * from cruiseHistory")
     HistoryArr = query.cursor.fetchall()
-    query = conn.execute("select itemID, numberSold from cruiseHistory;")
+    query = conn.execute("select itemID, numberSold, revenue from cruiseHistory;")
     result = {'history': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
     return jsonify(result)
 
